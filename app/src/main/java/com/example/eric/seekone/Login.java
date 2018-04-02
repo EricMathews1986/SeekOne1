7fc6dbc06b351360c739806b6e3031cc098f8ce7
EricMathews1986 <37989125+EricMathews1986@users.noreply.github.com>
2018-04-01 21:19:22 -0400

    Initial commit

---
+++ app/src/main/java/com/example/eric/seekone/Login.java
@@ -0,0 +1,19 @@
+package com.example.eric.seekone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    view view
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        view = this.getWindow().getDecorView();
+        view
+
+
+}
